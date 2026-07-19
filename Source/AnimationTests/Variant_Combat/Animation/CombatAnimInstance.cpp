c7a72c3783e67d3d4f606150ea9b2eb719a28d44
Daniel Goldaraz <dgoldaraz@gmail.com>
2026-07-19 16:02:02 +0200

    Add Proxy Logic
    Calculate different values on Character
    Create Proxy and read those values

---
+++ Source/AnimationTests/Variant_Combat/Animation/CombatAnimInstance.cpp
@@ -0,0 +1,37 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CombatAnimInstance.h"
+
+#include "CombatCharacter.h"
+
+void FProxyCombatGameplayData::PreUpdate(UAnimInstance* InAnimInstance, float DeltaSeconds)
+{
+	FAnimInstanceProxy::PreUpdate(InAnimInstance, DeltaSeconds);
+	
+	if (InAnimInstance != nullptr)
+	{
+		// Update Proxy values like - BIsAttacking, Yaw Delta rotation and horizontal Speed
+		if (ACombatCharacter* CombatCharacter = Cast<ACombatCharacter>(InAnimInstance->GetOwningActor()))
+		{
+			bIsAttacking = CombatCharacter->GetIsAttacking();
+			// Define how much the character should lean left or right
+			YawDelta = CombatCharacter->GetYawDelta();
+			// define if the character needs to lean at all
+			HorizontalVelocity = CombatCharacter->GetHorizontalSpeed();
+		}
+	}
+}
+
+void UCombatAnimInstance::NativeThreadSafeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeThreadSafeUpdateAnimation(DeltaSeconds);
+	
+	// Calculate variables based on proxy
+	// Calculate Lean based on proxy values
+}
+
+FAnimInstanceProxy* UCombatAnimInstance::CreateAnimInstanceProxy()
+{
+	return new FProxyCombatGameplayData(this);
+}
---
+++ Source/AnimationTests/Variant_Combat/Animation/CombatAnimInstance.h
@@ -0,0 +1,69 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "Animation/AnimInstanceProxy.h"
+#include "CombatAnimInstance.generated.h"
+
+/**
+ * Custom Anim Instance class.
+ * This example allow to create a custom combat anim instance with specific logic
+ * One new logic added will be to create a Thread-Safe Data Proxy that store relevant info
+ * 
+ */
+
+/** Proxy override for this UAnimInstance-derived class */
+USTRUCT()
+struct FProxyCombatGameplayData : public FAnimInstanceProxy
+{
+	GENERATED_BODY()
+
+public:
+	FProxyCombatGameplayData()
+	{
+	}
+
+	FProxyCombatGameplayData(UAnimInstance* InAnimInstance)
+		: FAnimInstanceProxy(InAnimInstance)
+	{
+	}
+	
+	virtual void PreUpdate(UAnimInstance* InAnimInstance, float DeltaSeconds) override;
+	
+	bool bIsAttacking = false;
+	float YawDelta = 0.0f;
+	float HorizontalVelocity = 0.0f;
+	
+};
+
+UCLASS()
+class ANIMATIONTESTS_API UCombatAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+	
+	// Return Custom Proxy
+	FProxyCombatGameplayData* GetControlRigProxyOnGameThread() { return &GetProxyOnGameThread <FProxyCombatGameplayData>(); }
+	
+	virtual void NativeThreadSafeUpdateAnimation(float DeltaSeconds) override;
+protected:
+
+	// Create custom Proxy
+	virtual FAnimInstanceProxy* CreateAnimInstanceProxy() override;
+	
+	// Define how much we should Lean
+	float LeanValue = 0.0f;
+	
+	// Clamp rotation based on this float. This float controls how quickly the character reaches the Max Lean.
+	UPROPERTY(EditAnywhere, Category = "Lean")
+	float LeanClamp = 300.0f;
+	
+	// Blend lean based on this value. This value defines when Lean will be applied (smoothly) based on Horizontal Speed
+	UPROPERTY(EditAnywhere, Category = "Lean")
+	float LeanBlend = 200.0f;
+	
+	// Speed of blend when calculating rotation, so sudden changes of rotation doesn't drive animation too hard
+	UPROPERTY(EditAnywhere, Category = "Lean")
+	float RotationBlendSpeed = 6.0f;
+};
