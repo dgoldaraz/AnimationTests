ee6d4a825f77300f9bf5e8e390220a56c4c083d1
Daniel Goldaraz <dgoldaraz@gmail.com>
2026-07-22 23:38:09 +0200

    Add bew inertial Bone first iteration
    Add AnimGraph and AnimNode
    Create and update bone based on character Velocity using sprint-damper logic

---
+++ Source/AnimationTests/Animation/Nodes/AnimGraphNode_IntertialOffset.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Editor/AnimGraph/Public/AnimGraphNode_SkeletalControlBase.h"
+#include "AnimGraphNode_IntertialOffset.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ANIMATIONTESTS_API UAnimGraphNode_IntertialOffset : public UAnimGraphNode_SkeletalControlBase
+{
+	GENERATED_BODY()
+};
---
+++ Source/AnimationTests/Animation/Nodes/AnimNode_InertialOffset.h
@@ -0,0 +1,21 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/ObjectMacros.h"
+#include "BoneContainer.h"
+#include "BonePose.h"
+#include "BoneControllers/AnimNode_SkeletalControlBase.h"
+#include "AnimNode_IntertialOffset.generated.h"
+
+class USkeletalMeshComponent;
+
+/**
+ * Custom node that apply offset to a bone based on character speed, simulating inertia
+ */
+USTRUCT()
+struct ANIMATIONTESTS_API FAnimNode_IntertialOffset : public FAnimNode_SkeletalControlBase
+{
+	GENERATED_BODY()
+};
